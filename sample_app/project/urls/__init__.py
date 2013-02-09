a5b7279621d6326467d808ae90eaa6437f0c07a3
Jj <jjdelc@gmail.com>
2013-02-09 14:05:06 -0500

    App working

--- sample_app/project/urls/__init__.py
+++ sample_app/project/urls/__init__.py
@@ -1,15 +1,14 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sample_app.views.home', name='home'),
-    # url(r'^sample_app/', include('sample_app.foo.urls')),
+    url(r'^', include('myapp.urls'))
 )
 
-from django.views.static import serve
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
