c91dd6c8292306a8e9a1975c2a3150dcf75b363f
Willy Aguirre <marti1125@gmail.com>
2013-02-09 18:16:29 -0500

    update app.js

--- sample_app/static/js/app.js
+++ sample_app/static/js/app.js
@@ -1,6 +1,10 @@
 var signinLink = document.getElementById('signin');
 var signoutLink = document.getElementById('signout');
 
+function veryfyAssertion(assertion){
+	document.location.href = window.location + assertion;
+}
+
 signinLink.onclick = function() {
   navigator.id.request();
 };
@@ -8,3 +12,13 @@ signinLink.onclick = function() {
 signoutLink.onclick = function() {
   navigator.id.logout();
 };
+
+navigator.id.watch({
+	onlogin: function(assertion){
+		veryfyAssertion(assertion);
+	},
+	onlogout: function(){
+		
+	}
+});
+
